38f4b46fa60170b1a5779ca5f4835609d7389028
apple <oladipupo@errandpay.com>
2025-09-09 22:13:11 +0100

    add comments to the code

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,24 +5,27 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
-    private float timer = 1.5f;
+    private float timer = 1.0f;
     private readonly float dogSpawnInterval = 1.0f;
 
     // Update is called once per frame
     void Update()
     {
+        // this keeps track of how much time has elapsed
         CountTime();
 
         // On spacebar press, send dog
         if (timer >= dogSpawnInterval && Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            // reset the timer after instantiation
             timer = 0;
         }
     }
 
     void CountTime()
     {
+        // this check ensures the timer does not keep counting up infinitely
         if (timer < dogSpawnInterval)
         {
             timer += Time.deltaTime;
